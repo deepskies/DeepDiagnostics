8f7f47ef63d735b761db34c1c6d8eb484188d685
scarletnorberg <scarletnorberg@gmail.com>
2025-09-18 16:47:53 +0000

    Changing this to include as the function in import

--- src/deepdiagnostics/metrics/__init__.py
+++ src/deepdiagnostics/metrics/__init__.py
@@ -1,7 +1,7 @@
 from deepdiagnostics.metrics.all_sbc import AllSBC
 from deepdiagnostics.metrics.coverage_fraction import CoverageFraction
 from deepdiagnostics.metrics.local_two_sample import LocalTwoSampleTest as LC2ST
-from deepdiagnostics.metrics.under_cdf_parity import CDFParityAreaUnderCurve
+from deepdiagnostics.metrics.under_cdf_parity import CDFParityAreaUnderCurve as CDFParityAreaUnderCurve
 
 def void(*args, **kwargs): 
     def void2(*args, **kwargs):
